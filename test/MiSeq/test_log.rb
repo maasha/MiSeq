923193d7d4d9f104286b2d29829cfb231d9bb8ec
Martin Asser Hansen <mail@maasha.dk>
2015-08-20 13:03:58 +0200

    test_log ok

---
+++ test/MiSeq/test_log.rb
@@ -0,0 +1,49 @@
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', '..')
+
+require 'test/helper'
+require 'lib/miseq'
+
+# rubocop:disable ClassLength
+
+# Test class for MiSeq.
+class TestLog < Test::Unit::TestCase
+  def setup
+    @dir_src  = Dir.mktmpdir('miseq_src')
+    @dir_dst  = Dir.mktmpdir('miseq_dst')
+    @file_log = File.join(@dir_src, 'log.txt')
+  end
+
+  def teardown
+    FileUtils.rm_rf @dir_src
+  end
+
+  test 'Log#log new log file works OK' do
+    logger = MiSeq::Log.new(@file_log)
+    logger.log('my message')
+
+    lines = []
+
+    File.open(@file_log) do |ios|
+      ios.each_line { |line| lines << line.chomp }
+    end
+
+    assert_equal(1, lines.size)
+    assert_equal('my message', lines.first.split("\t").last)
+  end
+
+  test 'Log#log appends works OK' do
+    logger = MiSeq::Log.new(@file_log)
+    logger.log('1 message')
+    logger.log('2 message')
+
+    lines = []
+
+    File.open(@file_log) do |ios|
+      ios.each_line { |line| lines << line.chomp }
+    end
+
+    assert_equal(2, lines.size)
+    assert_equal('1 message', lines.first.split("\t").last)
+    assert_equal('2 message', lines.last.split("\t").last)
+  end
+end
