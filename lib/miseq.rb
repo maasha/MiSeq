c860677af94eac99ea2083ff5f78f7c790080359
Martin Asser Hansen <mail@maasha.dk>
2015-08-20 16:21:22 +0200

    Added qc and fileutils

--- lib/miseq.rb
+++ lib/miseq.rb
@@ -2,9 +2,11 @@
 module MiSeq
   require 'English'
   require 'pp'
+  require 'fileutils'
   require 'MiSeq/data'
   require 'MiSeq/data_dir'
   require 'MiSeq/log'
   require 'MiSeq/run_statistics'
   require 'MiSeq/sample_sheet'
+  require 'MiSeq/qc'
 end
